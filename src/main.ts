56f88b6ccc3b6d4d8091348c1318a8d06cf6cf43
evelbulgroz <uhg@ulrikgade.dk>
2024-11-01 13:37:12 +0100

    Copy over main.ts from old project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@evelbulgroz/ddd-base';
+
+import { AppConfig } from 'src/domain/config-options.model';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const logger = app.get(Logger);
+
+	// Set global prefix for all routes (from the configuration)
+	const prefix = configService.get('app.globalprefix');
+	app.setGlobalPrefix(prefix);
+
+	// Get the port from the configuration
+	const appConfig = configService.get('app') as AppConfig;
+	const port = appConfig.baseURL?.port;
+
+	// Start the application
+	await app.listen(port);
+	logger.log(`Application is running on: ${await app.getUrl()}/${prefix}`);//, 'Bootstrap');
+
+	// Handle SIGINT signal, i.e. Ctrl+C in terminal
+	process.on('SIGINT', async () => {
+		logger.log('SIGINT signal received: closing the application...');//, 'Bootstrap');
+		await app.close();
+		process.exit(0); // exit process immediately
+	});
 }
 bootstrap();
