d4805657354e892310b17537bf254229eb615bd4
evelbulgroz <uhg@ulrikgade.dk>
2024-11-23 17:02:11 +0100

    Scaffold overrides to event creation methods

--- src/repositories/conditioning-log.repo.ts
+++ src/repositories/conditioning-log.repo.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@nestjs/common";
 
 import { Observable } from "rxjs";
+import { v4 as uuidv4 } from 'uuid';
 
 import { ConditioningLog } from "../domain/conditioning-log.entity";
 import { ConditioningLogDTO } from "../dtos/conditioning-log.dto";
-import { EntityId, Logger, Result } from "@evelbulgroz/ddd-base";
+import { EntityCreatedEvent, EntityCreatedEventDTO, EntityDeletedEvent, EntityDeletedEventDTO, EntityDTO, EntityId, EntityUpdatedEvent, EntityUpdatedEventDTO, Logger, Result } from "@evelbulgroz/ddd-base";
 import { TrainingLogRepo } from "@evelbulgroz/fitnessapp-base";
 import { Query } from "@evelbulgroz/query-fns";
 
@@ -39,21 +40,72 @@ export class ConditioningLogRepo<T extends ConditioningLog<T,U>, U extends Condi
 			default:
 				return Result.fail<any>(`Unknown or unsupported log type: ${className}`);
 		}
-	}	
+	}
+
+	//---------------------------- PUBLIC METHODS ---------------------------//
 	
-
-
 	/** Fetch entities by query criteria
 	 * @param criteria The query criteria to use to filter entities
 	 * @param matchAll If true, all criteria must match; otherwise, any criteria matching is sufficient
 	 * @returns A Result wrapping an Observable of the entities matching the query criteria
 	 * @remark Exists to enforce use of ConditioningLogQueryCriteria, otherwise delegates to base class
+	 * @todo Consider if this is still needed now that the logs method takes a query
 	 */
 	public async fetchByQuery(criteria: Query<any,any>, matchAll: boolean = false): Promise<Result<Observable<ConditioningLog<T,U>[]>>> {
 		throw new Error("Method not implemented: implement in concrete subclass");
 	}
+
+	//------------------------ PROTECTED METHODS ------------------------//
 	
-	//--------------------------------- PLACEHOLDERS -------------------------------------
+	/** Create log created event
+	 * @param log The log to create the event for
+	 * @returns The log created event
+	 * @todo Refactor to return domain specific event (e.g. LogCreatedEvent)
+	 * @todo Add override modifier after updating base class library
+	 */
+	protected createEntityCreatedEvent(log?: T): EntityCreatedEvent<EntityCreatedEventDTO<any>, EntityDTO> {
+		return new EntityCreatedEvent(<EntityCreatedEventDTO<EntityDTO>>{
+			eventId: uuidv4(),
+			eventName: 'EntityCreatedEvent',
+			occurredOn: (new Date()).toUTCString(),
+			payload: log?.toJSON() as EntityDTO
+		});
+	}
+
+	/** Create log updated event
+	 * @param log The log to create the event for
+	 * @returns The log updated event
+	 * @todo Refactor to return domain specific event (e.g. LogUpdatedEvent)
+	 * @todo Add override modifier after updating base class library
+	 */
+	protected createEntityUpdatedEvent(log?: T): EntityUpdatedEvent<EntityUpdatedEventDTO<any>, EntityDTO> {
+		return new EntityUpdatedEvent(<EntityUpdatedEventDTO<EntityDTO>>{
+			eventId: uuidv4(),
+			eventName: 'EntityUpdatedEvent',
+			occurredOn: (new Date()).toUTCString(),
+			payload: log?.toJSON() as Partial<EntityDTO>
+		});
+	}
+
+	/** Create log deleted event
+	 * @param id The log id to create the event for
+	 * @returns The log deleted event
+	 * @todo Refactor to return domain specific event (e.g. LogDeletedEvent)
+	 * @todo Add override modifier after updating base class library
+	 */
+	protected createEntityDeletedEvent(id?: EntityId): EntityDeletedEvent<EntityDeletedEventDTO<any>, EntityDTO> {
+		return new EntityDeletedEvent(<EntityDeletedEventDTO<EntityDTO>>{
+			eventId: uuidv4(),
+			eventName: 'EntityDeletedEvent',
+			occurredOn: (new Date()).toUTCString(),
+			payload: {entityId: id } as Partial<EntityDTO>
+		});
+	}
+
+	//---------------------------- PLACEHOLDERS -----------------------------//
+
+	// NOTE: These methods are placeholders for the template methods that must be implemented in a concrete subclass:
+	// since this class is not marked abstract, the compiler insists they must be also implemented here.
 
 	protected initializePersistence(): Promise<Result<void>> { throw new Error("Method not implemented: implement in concrete subclass");}
 	protected populateEntityCache(): Promise<Result<void>> { throw new Error("Method not implemented: implement in concrete subclass"); }
--- src/repositories/user-repo.model.ts
+++ src/repositories/user-repo.model.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common";
 
 import { Observable } from "rxjs";
+import { v4 as uuidv4 } from 'uuid';
 
 import { User } from "../domain/user.entity";
 import { UserDTO } from "../dtos/user.dto";
 import { Query } from "@evelbulgroz/query-fns";
 
-import { EntityId, Repository, Result } from "@evelbulgroz/ddd-base";
+import { EntityCreatedEvent, EntityCreatedEventDTO, EntityDeletedEvent, EntityDeletedEventDTO, EntityDTO, EntityId, EntityUpdatedEvent, EntityUpdatedEventDTO, Repository, Result } from "@evelbulgroz/ddd-base";
 
-/**@classdesc Describes and provides default features for any User repository
+/** Notionally abstract class that describes and provides default features for any User repository
  * @remark Exists mostly to enable clients to depend on abstractions rather than a specific implementations
  * @remark Clients should inject this class and depend on the injector to provide the concrete implementation at runtime
  * @remark NestJS's DI system cannot inject abstract classes, so this class is not marked abstract though it should be treated as such
@@ -17,9 +18,7 @@ import { EntityId, Repository, Result } from "@evelbulgroz/ddd-base";
 @Injectable()
 export class UserRepository<T extends User, U extends UserDTO> extends Repository<User, UserDTO> {
 	
-	//--------------------------------- PUBLIC METHODS ---------------------------------
-	
-	//----------------------------------- PUBLIC STATIC METHODS -----------------------------------
+	//------------------------ PUBLIC STATIC METHODS ------------------------//
 
 	/** Get the class constructor from a class name
 	 * @param className The name of the class to get
@@ -35,10 +34,10 @@ export class UserRepository<T extends User, U extends UserDTO> extends Repositor
 			default:
 				return Result.fail<any>(`Unknown or unsupported user type: ${className}`);
 		}
-	}	
+	}
+
+	//---------------------------- PUBLIC METHODS ---------------------------//
 	
-
-
 	/** Fetch entities by query criteria
 	 * @param criteria The query criteria to use to filter entities
 	 * @param matchAll If true, all criteria must match; otherwise, any criteria matching is sufficient
@@ -47,9 +46,59 @@ export class UserRepository<T extends User, U extends UserDTO> extends Repositor
 	 */
 	public async fetchByQuery(criteria: Query<any,any>, matchAll: boolean = false): Promise<Result<Observable<User[]>>> {
 		throw new Error("Method not implemented: implement in concrete subclass");
+	}	
+	
+	//-------------------------- PROTECTED METHODS --------------------------//
+
+	/** Create user created event
+	 * @param user The user to create the event for
+	 * @returns The user created event
+	 * @todo Refactor to return domain specific event (e.g. UserCreatedEvent)
+	 * @todo Add override modifier after updating base class library
+	 */
+	protected createEntityCreatedEvent(user?: T): EntityCreatedEvent<EntityCreatedEventDTO<any>, EntityDTO> {
+		return new EntityCreatedEvent(<EntityCreatedEventDTO<EntityDTO>>{
+			eventId: uuidv4(),
+			eventName: 'EntityCreatedEvent',
+			occurredOn: (new Date()).toUTCString(),
+			payload: user?.toJSON() as EntityDTO
+		});
+	}
+
+	/** Create user updated event
+	 * @param user The user to create the event for
+	 * @returns The user updated event
+	 * @todo Refactor to return domain specific event (e.g. UserUpdatedEvent)
+	 * @todo Add override modifier after updating base class library
+	 */
+	protected createEntityUpdatedEvent(user?: T): EntityUpdatedEvent<EntityUpdatedEventDTO<any>, EntityDTO> {
+		return new EntityUpdatedEvent(<EntityUpdatedEventDTO<EntityDTO>>{
+			eventId: uuidv4(),
+			eventName: 'EntityUpdatedEvent',
+			occurredOn: (new Date()).toUTCString(),
+			payload: user?.toJSON() as Partial<EntityDTO>
+		});
+	}
+
+	/** Create user deleted event
+	 * @param id The user id to create the event for
+	 * @returns The user deleted event
+	 * @todo Refactor to return domain specific event (e.g. UserDeletedEvent)
+	 * @todo Add override modifier after updating base class library
+	 */
+	protected createEntityDeletedEvent(id?: EntityId): EntityDeletedEvent<EntityDeletedEventDTO<any>, EntityDTO> {
+		return new EntityDeletedEvent(<EntityDeletedEventDTO<EntityDTO>>{
+			eventId: uuidv4(),
+			eventName: 'EntityDeletedEvent',
+			occurredOn: (new Date()).toUTCString(),
+			payload: {entityId: id } as Partial<EntityDTO>
+		});
 	}
 	
-	//--------------------------------- PLACEHOLDERS -------------------------------------
+	//-------------------------- PLACEHOLDERS FOR TEMPLATE METHODS ------------------------------//
+
+	// NOTE: These methods are placeholders for the template methods that must be implemented in a concrete subclass:
+	// since this class is not marked abstract, the compiler insists they must be also implemented here.
 
 	protected initializePersistence(): Promise<Result<void>> { throw new Error("Method not implemented: implement in concrete subclass");}
 	protected populateEntityCache(): Promise<Result<void>> { throw new Error("Method not implemented: implement in concrete subclass"); }
