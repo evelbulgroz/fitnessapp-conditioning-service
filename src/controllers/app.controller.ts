5ae061418d2163c495d61f241cbc949379ea04f0
evelbulgroz <uhg@ulrikgade.dk>
2024-11-24 22:37:41 +0100

    Edit comments

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -211,6 +211,8 @@ export class AppController {
 	/* Delete a user here when the corresponding user is deleted in the user microservice
 	 * throws UnauthorizedException if requester is not user microservice
 	 * @todo Implement this method in user service
+	 * @remark Should use soft delete to preserve logs until periodic cleanup; this may require changes to Repository base class
+	 * @remark Restoration of deleted users beyond a certain time period will rely on backups 
 	*/
 	/*
 	@Delete(':id')
