69fbbd87dbb75dfa7745d61d7b1bc6f595d9f56b
evelbulgroz <uhg@ulrikgade.dk>
2024-12-31 15:32:38 +0100

    Start testing ConditioningLogRepo (in progress)

--- src/repositories/conditioning-log.repo.ts
+++ src/repositories/conditioning-log.repo.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 
 import { Observable } from "rxjs";
 import { v4 as uuidv4 } from 'uuid';
@@ -9,6 +9,7 @@ import {
 	EntityDeletedEvent,
 	EntityDeletedEventDTO,
 	EntityDTO,
+	EntityFactory,
 	EntityId,
 	EntityMetadataDTO,
 	EntityUpdatedEvent,
@@ -31,48 +32,21 @@ import { LogUpdatedEvent } from "../events/log-updated.event";
 /**@classdesc Concrete implementation of an injectable ConditioningLogRepo that uses an adapter to interact with a persistence layer */
 @Injectable()
 export class ConditioningLogRepo<T extends ConditioningLog<T,U>, U extends ConditioningLogDTO> extends TrainingLogRepo<ConditioningLog<T,U>, U> {
-	//---------------------------- CONSTRUCTOR ---------------------------//
+	//------------------------------ CONSTRUCTOR ----------------------------//
 
 	public constructor(
 		protected readonly adapter: PersistenceAdapter<ConditioningLogPersistenceDTO<U, EntityMetadataDTO>>,
 		protected readonly logger: Logger,
-		protected readonly throttleTime?: number
+		@Inject('REPOSITORY_THROTTLETIME') throttleTime: number, // todo: maybe get this from config
+		//protected readonly throttleTime?: number
 	) {
 		super(adapter, logger, throttleTime);
 	}
 	
-	//------------------------ PUBLIC STATIC METHODS ------------------------//
+	//------------------------------ PUBLIC API -----------------------------//
 
-	/** Get the class constructor from a class name
-	 * @param className The name of the class to get
-	 * @returns A Result wrapping the class constructor if successful, otherwise a failure result
-	 * @remark Exists to enable the generic creation of User entities from DTOs, while staying DRY
-	 * @remark Placeholder until base class is refactored to use a public static rather than a protected method
-	 */
-	public static getClassFromName(className: string): Result<any> {
-		switch (className) {
-			case 'ConditioningLog':
-				return Result.ok<any>(ConditioningLog);
-			// Add more cases as needed
-			default:
-				return Result.fail<any>(`Unknown or unsupported log type: ${className}`);
-		}
-	}
-
-	//---------------------------- PUBLIC METHODS ---------------------------//
+	// NOTE: Public API methods are inherited from the base class
 		
-	
-	/** Fetch entities by query criteria
-	 * @param criteria The query criteria to use to filter entities
-	 * @param matchAll If true, all criteria must match; otherwise, any criteria matching is sufficient
-	 * @returns A Result wrapping an Observable of the entities matching the query criteria
-	 * @remark Exists to enforce use of ConditioningLogQueryCriteria, otherwise delegates to base class
-	 * @todo Consider if this is still needed now that the logs method takes a query
-	 */
-	public async fetchByQuery(criteria: Query<any,any>, matchAll: boolean = false): Promise<Result<Observable<ConditioningLog<T,U>[]>>> {
-		throw new Error("Method not implemented: implement in concrete subclass");
-	}
-
 	//-------------------------- PROTECTED METHODS --------------------------//
 	
 	// initialize the repository
@@ -113,74 +87,30 @@ export class ConditioningLogRepo<T extends ConditioningLog<T,U>, U extends Condi
 
 	//------------------- TEMPLATE METHOD IMPLEMENTATIONS -------------------//	
 
-	// todo: remove once we have a better method for importing data
+	// todo: figure out a better way to handle imports
 	protected async finalizeInitialization(): Promise<Result<void>> {
 		this.logger.log(`${this.constructor.name}: Finalizing initialization...`);
 		//await this.#subscribeToImportUpdates();
 		this.logger.log(`${this.constructor.name}: Initialization complete.`);
 		return Promise.resolve(Result.ok<void>());
 	}
-
-	// NOTE: Unless using more specfic event names, just use the base class methods and remove these overrides
 	
-	/** Create log created event
-	 * @param log The log to create the event for
-	 * @returns The log created event
-	 */
-	protected override createEntityCreatedEvent(log?: T): EntityCreatedEvent<EntityCreatedEventDTO<any>, EntityDTO> {
-		const event = new LogCreatedEvent({
-			eventId: uuidv4(),
-			eventName: 'LogCreatedEvent',
-			occurredOn: (new Date()).toUTCString(),
-			payload: log?.toDTO() as ConditioningLogDTO
-		});
-
-		return event as any; // todo: sort out the generics later
-	}
-
-	/** Create log updated event
-	 * @param log The log to create the event for
-	 * @returns The log updated event
-	 */
-	protected override createEntityUpdatedEvent(log?: T): EntityUpdatedEvent<EntityUpdatedEventDTO<any>, EntityDTO> {
-		const event = new LogUpdatedEvent({
-			eventId: uuidv4(),
-			eventName: 'LogUpdatedEvent',
-			occurredOn: (new Date()).toUTCString(),
-			payload: log?.toDTO() as ConditioningLogDTO
-		});
-
-		return event as any; // todo: sort out the generics later
-	}
-
-	/** Create log deleted event
-	 * @param id The log id to create the event for
-	 * @returns The log deleted event
-	 */
-	protected override createEntityDeletedEvent(id?: EntityId): EntityDeletedEvent<EntityDeletedEventDTO<any>, EntityDTO> {
-		const event = new LogDeletedEvent({
-			eventId: uuidv4(),
-			eventName: 'LogDeletedEvent',
-			occurredOn: (new Date()).toUTCString(),
-			payload: { entityId: id } as ConditioningLogDTO
-		});
-
-		return event as any; // todo: sort out the generics later
-	}
-
 	protected getEntityFromDTO(dto: U): T | undefined {
-		return this.cache.value.find(e => { 
-			if (typeof dto.entityId === 'string' || typeof dto.entityId === 'number') { // try to match by id, when possible and valid
-				return e.entityId === dto.entityId;
-			}
-			else { // try to match by source id
+		return (this.getCachedEntityById(dto.entityId!) ?? // try to find the entity by ID in the cache
+			this.cache.value.find(e => { // try to find the entity by source ID in the cache
 				return (e.meta?.sourceId?.id === dto.meta?.sourceId?.id && e.meta?.sourceId?.source === dto.meta?.sourceId?.source);
-			}
-		}) as T | undefined;
+		})) as T | undefined;
 	}
 
 	protected getClassFromDTO(dto: U): Result<any> {
-		return ConditioningLogRepo.getClassFromName(dto.className);
+		const className = dto.className;
+		switch (className) {
+			case 'ConditioningLog':
+				return Result.ok<any>(ConditioningLog);
+			// Add more cases as needed
+			default:
+				return Result.fail<any>(`Unknown or unsupported log type: ${className}`);
+		}
 	}
 
 	//----------------------- OTHER PROTECTED METHODS -----------------------//
@@ -194,17 +124,21 @@ export class ConditioningLogRepo<T extends ConditioningLog<T,U>, U extends Condi
 	 * @remark Exists to enable the generic creation of User entities from DTOs, while staying DRY
 	 * @todo Remove when implemented in ddd-base library
 	 */
-	protected createEntityFromPersistenceDTO(dto: ConditioningLogPersistenceDTO<U, EntityMetadataDTO>, id?: EntityId, overView: boolean = true): Result<T> {
-		const result = this.getClassFromDTO(dto);
-		if (result.isFailure) {
+	protected createEntityFromPersistenceDTO(dto: ConditioningLogPersistenceDTO<U, EntityMetadataDTO>, id?: EntityId, ...args: any[]): Result<T> {
+		const factoryResult = this.getFactoryFromDTO(dto);
+		if (factoryResult.isFailure) {
 			return Result.fail<T>(`Unknown or unsupported entity type: ${dto.className}`);
 		}
-		const ClassRef = result.value as typeof ConditioningLog;
-		const metadataDTO = ClassRef.getMetaDataDTO(dto);
-		if (id) { // assign the id if provided
-			dto.entityId = id;
+		const factory = factoryResult.value as EntityFactory<T, U>;
+
+		const classResult = this.getClassFromDTO(dto);
+		if (classResult.isFailure) {
+			return Result.fail<T>(classResult.error);
 		}
-		const createResult = ClassRef.create(dto, metadataDTO, overView);
+		const ClassRef = classResult.value as typeof ConditioningLog;				
+		const metadataDTO = ClassRef.getMetaDataDTO(dto);
+		
+		const createResult = factory(dto, metadataDTO, args);
 		if (createResult.isFailure) {
 			return Result.fail<T>(`Failed to create entity: ${createResult.error}`);
 		}
