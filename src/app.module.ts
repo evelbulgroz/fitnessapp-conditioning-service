a9f82d9efd62c0e04c7d81012e02496e07a4885f
evelbulgroz <uhg@ulrikgade.dk>
2025-02-16 08:20:32 +0100

    Retire default app controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 
 import { AggregationQueryMapper } from './mappers/aggregation-query.mapper';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConditioningController } from './controllers/conditioning.controller';
 import { QueryMapper } from './mappers/query.mapper';
 import { FsPersistenceAdapterService } from './repositories/adapters/fs-persistence-adapter/fs-persistence-adapter.service';
@@ -13,13 +11,11 @@ import { UserController } from './controllers/user.controller';
 @Module({
 	imports: [],
 	controllers: [
-		AppController,
 		ConditioningController,
 		UserController
 	],
 	providers: [
 		AggregationQueryMapper,
-		AppService,
 		QueryMapper,
 		{
 			provide: PersistenceAdapter,
