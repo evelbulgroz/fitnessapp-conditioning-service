1e9dda04deb5ba95b98d8382f93b097d40b7ad24
evelbulgroz <uhg@ulrikgade.dk>
2025-03-09 07:49:11 +0100

    Move auth to own module (in progress)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,17 @@ import { Global, Module }  from '@nestjs/common';
 
 import { ConsoleLogger, Logger }  from '@evelbulgroz/ddd-base';
 
-import BcryptCryptoService from './shared/services/authentication/crypto/bcrypt-crypto.service';
+import AuthenticationModule from './authentication/authentication.module';
 import ConditioningController  from './conditioning/controllers/conditioning.controller';
 import ConditioningModule from './conditioning/conditioning.module';
-import CryptoService from './shared/services/authentication/crypto/models/crypto-service.model';
 import EventDispatcherService  from './shared/services/utils/event-dispatcher/event-dispatcher.service';
 import JwtAuthStrategy from './infrastructure/strategies/jwt-auth.strategy';
-import JwtSecretService from './shared/services/authentication/jwt/jwt-secret.service';
-import JwtService  from './shared/services/authentication/jwt/models/jwt-service.model';
-import JsonWebtokenService  from './shared/services/authentication/jwt/json-webtoken.service';
 import UserController  from './user/controllers/user.controller';
 import UserModule from './user/user.module';
 
 import productionConfig from './../config/production.config';
 import developmentConfig from '../config/development.config';
+
 
 // todo: Copy over (de)registration logic from API Gateway to be able to effectively authenticate and collaborate with other microservices
 @Global()
@@ -35,6 +32,7 @@ import developmentConfig from '../config/development.config';
 		}),
 		ConditioningModule,
 		UserModule,
+		AuthenticationModule,
 	],
 	controllers: [
 		ConditioningController,
@@ -42,27 +40,7 @@ import developmentConfig from '../config/development.config';
 	],
 	providers: [		
 		ConfigService,
-		{
-			provide: CryptoService,
-			useClass: BcryptCryptoService,
-		},
 		EventDispatcherService,
-		JwtAuthStrategy,		
-		{
-			provide: JwtService,
-			useFactory: (secretService: JwtSecretService) => {
-				return new JsonWebtokenService(secretService);
-			},
-			inject: [JwtSecretService],
-		},
-		{
-			provide: JwtSecretService,
-			useFactory: (configService: ConfigService) => {
-				const secret = configService.get<string>('security.authentication.jwt.accessToken.secret') ?? 'secret-not-found';
-				return new JwtSecretService(secret);
-			},
-			inject:	[ConfigService],
-		},
 		{
 			provide: Logger,
 			useClass: ConsoleLogger,
---
+++ src/authentication/authentication.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import BcryptCryptoService from './crypto/bcrypt-crypto.service';
+import CryptoService from './crypto/models/crypto-service.model';
+import JwtAuthStrategy from '../infrastructure/strategies/jwt-auth.strategy';
+import JwtSecretService from './jwt/jwt-secret.service';
+import JwtService from './jwt/models/jwt-service.model';
+import JsonWebtokenService from './jwt/json-webtoken.service';
+
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: CryptoService,
+      useClass: BcryptCryptoService,
+    },
+    JwtAuthStrategy,
+    {
+      provide: JwtService,
+      useFactory: (secretService: JwtSecretService) => {
+        return new JsonWebtokenService(secretService);
+      },
+      inject: [JwtSecretService],
+    },
+    {
+      provide: JwtSecretService,
+      useFactory: (configService: ConfigService) => {
+        const secret = configService.get<string>('security.authentication.jwt.accessToken.secret') ?? 'secret-not-found';
+        return new JwtSecretService(secret);
+      },
+      inject: [ConfigService],
+    },
+	// later: add registration and token services
+  ],
+  exports: [
+    CryptoService,
+    JwtAuthStrategy,
+    JwtService,
+    JwtSecretService,
+  ],
+})
+export class AuthenticationModule {}
+export default AuthenticationModule;
