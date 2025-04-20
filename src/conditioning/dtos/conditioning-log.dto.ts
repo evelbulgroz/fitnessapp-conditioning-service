638455c5a75eeea8287fb5b14a93d70b05ec06e6
evelbulgroz <uhg@ulrikgade.dk>
2025-04-20 18:03:58 +0200

    Make ConditioningLogDTO a class

--- src/conditioning/dtos/conditioning-log.dto.ts
+++ src/conditioning/dtos/conditioning-log.dto.ts
@@ -1,9 +1,11 @@
 import { ConditioningLapDTO } from "./conditioning-lap.dto.js";
 import { TrainingLogDTO } from "@evelbulgroz/fitnessapp-base";
 
-export interface ConditioningLogDTO extends TrainingLogDTO {
+export class ConditioningLogDTO extends TrainingLogDTO {
 	/** Specifies a segment of a session, e.g. a lap in a pool/around a track, or a segment of a bike ride
 	 * @remark May be empty if no laps, or undefined if this is an overview
+	 * @see {@link TrainingLogDTO}
+	 * @see {@link EntityDTO}
 	 */
 	laps?: ConditioningLapDTO[];
 }
