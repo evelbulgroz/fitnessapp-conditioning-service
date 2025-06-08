a0a072e1853a2569a3453767d8c98c2f5f3c1cbc
evelbulgroz <uhg@ulrikgade.dk>
2025-06-08 15:42:09 +0200

    Retire AppState, AppStateInfo models

--- src/app-health/models/app-state-info.model.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentStateInfo } from '../../libraries/managed-stateful-component';
-
-/** Specifies the contents of an overall app health state entry with respect to readiness to serve requests.
- * @remarks The health status is used to determine if the application is lively, and/or healthy, and ready to serve requests.
- * @remarks For now, just an alias for ComponentStateInfo, but may be extended in the future.
- */
-
-export type AppStateInfo = ComponentStateInfo;
-export default AppStateInfo;
